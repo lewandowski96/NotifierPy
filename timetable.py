6c3721789f938820526121f434e412491afae9dc
Sanjeewa <ssanjeewa1996@gmail.com>
2024-01-01 12:55:47 +0530

    initial commit

---
+++ timetable.py
@@ -0,0 +1,171 @@
+def get_monday():
+
+    return [
+        {
+            'taskName': 'ML Course',
+            'taskHour':  7,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'Eat Breakfast',
+            'taskHour':  9,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Check Chats, Emails',
+            'taskHour':  9,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'Start Work!',
+            'taskHour':  9,
+            'taskMinute': 45
+        },
+        {
+            'taskName': 'Rest and Tea!',
+            'taskHour':  11,
+            'taskMinute': 45
+        },
+        {
+            'taskName': 'Work Again!',
+            'taskHour':  12,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Workout and Shower!',
+            'taskHour':  13,
+            'taskMinute': 15
+        },
+        {
+            'taskName': 'Lunch!',
+            'taskHour':  15,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Work Again!',
+            'taskHour':  15,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'Guitar Hero!',
+            'taskHour':  17,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Worship and Tea!',
+            'taskHour':  18,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Youtube Time!',
+            'taskHour':  18,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'Web Dev Course!',
+            'taskHour':  19,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'Dinner!',
+            'taskHour':  21,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Web Dev Resume!',
+            'taskHour':  21,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'TV TIME!!',
+            'taskHour':  23,
+            'taskMinute': 0
+        },
+    ]
+
+
+def get_tuesday():
+
+    return [
+        {
+            'taskName': 'ML Course',
+            'taskHour':  7,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'Eat Breakfast',
+            'taskHour':  9,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Check Chats, Emails',
+            'taskHour':  9,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'Start Work!',
+            'taskHour':  9,
+            'taskMinute': 45
+        },
+        {
+            'taskName': 'Rest and Tea!',
+            'taskHour':  11,
+            'taskMinute': 45
+        },
+        {
+            'taskName': 'Work Again!',
+            'taskHour':  12,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Workout and Shower!',
+            'taskHour':  13,
+            'taskMinute': 15
+        },
+        {
+            'taskName': 'Lunch!',
+            'taskHour':  15,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Work Again!',
+            'taskHour':  15,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'Guitar Hero!',
+            'taskHour':  17,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Worship and Tea!',
+            'taskHour':  18,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Youtube Time!',
+            'taskHour':  18,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'Flutter Course!',
+            'taskHour':  19,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'Dinner!',
+            'taskHour':  21,
+            'taskMinute': 0
+        },
+        {
+            'taskName': 'Flutter Resume!',
+            'taskHour':  21,
+            'taskMinute': 30
+        },
+        {
+            'taskName': 'TV TIME!!',
+            'taskHour':  23,
+            'taskMinute': 0
+        },
+    ]
+
